7c898a629996b7e823558525c56c4169566ad84c
Gabriel <gabrielfernandez090997@gmail.com>
2026-03-11 16:40:33 -0300

    Se agregó un nuevo navbar para las demas paginas

--- src/components/form/FormC.jsx
+++ src/components/form/FormC.jsx
@@ -1,35 +1,40 @@
-import { Button, Form } from "react-bootstrap";
+import { Button, Container, Form } from "react-bootstrap";
+import "./FormC.css";
 
 const FormC = () => {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicName">
-        <Form.Label>Nombre de usuario </Form.Label>
-        <Form.Control type="text" placeholder="Nombre" />
-      </Form.Group>
+    <>
+      <Container className="d-flex justify-content-center my-5">
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicName">
+            <Form.Label>Nombre de usuario </Form.Label>
+            <Form.Control type="text" placeholder="Nombre" />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email </Form.Label>
-        <Form.Control type="email" placeholder="Email" />
-      </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Correo </Form.Label>
+            <Form.Control type="email" placeholder="Correo" />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Contraseña</Form.Label>
-        <Form.Control type="password" placeholder="Contraseña" />
-      </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Contraseña</Form.Label>
+            <Form.Control type="password" placeholder="Contraseña" />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Repetir contraseña</Form.Label>
-        <Form.Control type="password" placeholder="Repetir contraseña" />
-      </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Repetir contraseña</Form.Label>
+            <Form.Control type="password" placeholder="Repetir contraseña" />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Enviar
+          </Button>
+        </Form>
+      </Container>
+    </>
   );
 };
 
