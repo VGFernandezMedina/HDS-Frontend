b8fec0ed6f869564e013062b5069dc09b75a09e2
Gabriel <gabrielfernandez090997@gmail.com>
2026-04-01 22:26:05 -0300

    Se agregó un nuevo menú en el navbar y nuevas condiciones

--- src/components/navbar/NavbarC.jsx
+++ src/components/navbar/NavbarC.jsx
@@ -1,17 +1,33 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavLink from "react-bootstrap/NavLink";
 import NavDropdown from "react-bootstrap/NavDropdown";
+/* import NavLink from "react-bootstrap/NavLink"; */
+
 import "./NavbarC.css";
 import { Image } from "react-bootstrap";
 import logoNavbar from "/favicon.png";
-import { useLocation } from "react-router-dom";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
 
 const NavbarC = () => {
-  const location = useLocation();
+  const token = JSON.parse(localStorage.getItem("token")) || null;
+  const rolUsuario = JSON.parse(localStorage.getItem("rol")) || null;
 
-  const isHome = location.pathname === "/";
+  const navigate = useNavigate();
+  const location = useLocation();
+  const isHome =
+    location.pathname === "/" || location.pathname === "/user"; /*  ||
+    location.pathname === "/admin" */
+
+  const handleLogoutUser = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("rol");
+    /* sessionStorage.removeItem("idUsuario"); */
+
+    setTimeout(() => {
+      navigate("/");
+    }, 500);
+  };
 
   return (
     <>
@@ -21,41 +37,66 @@ const NavbarC = () => {
         className={isHome ? "navbar-transparente" : "navbar-normal"}
       >
         <Container>
-          <NavLink href="/">
+          <NavLink
+            to={!token ? "/" : rolUsuario === "admin" ? "/admin" : "/user"}
+          >
             <Image className="logo-navbar" src={logoNavbar} />
           </NavLink>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <NavLink className="fuente-navbar" href="/">
+              <NavLink
+                className="fuente-navbar"
+                to={!token ? "/" : rolUsuario === "admin" ? "/admin" : "/user"}
+              >
                 Inicio
               </NavLink>
-              <NavLink className="fuente-navbar" href="#">
+              <NavLink className="fuente-navbar" to="#">
                 Tienda
               </NavLink>
-              <NavLink className="fuente-navbar" href="#">
+              <NavLink className="fuente-navbar" to="#">
                 Contacto
               </NavLink>
+              {token && rolUsuario === "admin" && (
+                <NavLink className="fuente-navbar" to="/user">
+                  Vista usuario
+                </NavLink>
+              )}
               <NavDropdown
-                className="fuente-navbar"
+                className="fuente-navbar px-0"
                 title="Equipos"
                 id="collapsible-nav-dropdown"
               >
-                <NavDropdown.Item href="#">Rocket League</NavDropdown.Item>
-                <NavDropdown.Item href="#">Dead by Daylight</NavDropdown.Item>
-                <NavDropdown.Item href="#">Fortnite</NavDropdown.Item>
-                <NavDropdown.Item href="#">Counter Strike</NavDropdown.Item>
-                <NavDropdown.Item href="#">Valorant</NavDropdown.Item>
-                <NavDropdown.Item href="#">League of legends</NavDropdown.Item>
+                <NavDropdown.Item to="#">Rocket League</NavDropdown.Item>
+                <NavDropdown.Item to="#">Dead by Daylight</NavDropdown.Item>
+                <NavDropdown.Item to="#">Fortnite</NavDropdown.Item>
+                <NavDropdown.Item to="#">Counter Strike</NavDropdown.Item>
+                <NavDropdown.Item to="#">Valorant</NavDropdown.Item>
+                <NavDropdown.Item to="#">League of legends</NavDropdown.Item>
               </NavDropdown>
             </Nav>
             <Nav>
-              <NavLink className="fuente-navbar" href="/login">
-                Iniciar Sesión
-              </NavLink>
-              <NavLink className="fuente-navbar" href="/register">
-                Registrarse
-              </NavLink>
+              {token ? (
+                <>
+                  {rolUsuario === "usuario" && (
+                    <NavLink className="fuente-navbar" to="#">
+                      Carrito
+                    </NavLink>
+                  )}
+                  <NavLink className="fuente-navbar" onClick={handleLogoutUser}>
+                    Cerrar Sesión
+                  </NavLink>
+                </>
+              ) : (
+                <>
+                  <NavLink className="fuente-navbar" to="/login">
+                    Iniciar Sesión
+                  </NavLink>
+                  <NavLink className="fuente-navbar" to="/register">
+                    Registrarse
+                  </NavLink>
+                </>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
