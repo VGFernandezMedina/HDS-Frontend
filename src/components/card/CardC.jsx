d8566ed6f3500b5f348965244f72a8901a232eb9
Gabriel <gabrielfernandez090997@gmail.com>
2026-04-06 16:15:10 -0300

    Se agregaron mas colores al proyecto y ShopPage recibe los datos del backend

--- src/components/card/CardC.jsx
+++ src/components/card/CardC.jsx
@@ -1,18 +1,25 @@
 import { Link } from "react-router-dom";
 import "./CardC.css";
-import Card from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import camiseta from "/camisetaHDS.jpg";
 
-const CardC = ({ idProd, urlImagen, titulo, descripcion, precio }) => {
+const CardC = ({ idProd, /* urlImagen, */ titulo, descripcion, precio }) => {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={urlImagen} />
+    <Card className="card-productos">
+      {/* <Card.Img variant="top" src={urlImagen} /> */}
+      <Card.Img variant="top" src={camiseta} />
       <Card.Body>
         <Card.Title>{titulo}</Card.Title>
-        <Card.Title>{precio}</Card.Title>
+        <Card.Title>${precio}</Card.Title>
         <Card.Text>{descripcion}</Card.Text>
-        <Link to={`/detalle-producto/${idProd}`} className="btn btn-primary">
-          Ver más
-        </Link>
+        <div className="botones-productos">
+          <Link to={`/detalle-producto/${idProd}`} className="btn-ver">
+            Ver detalles
+          </Link>
+          <Link to={`/detalle-producto/${idProd}`} className="btn-carrito">
+            Añadir al carrito
+          </Link>
+        </div>
       </Card.Body>
     </Card>
   );
--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,19 +1,54 @@
+import { useEffect, useState } from "react";
 import "./ShopPage.css";
 import { Col, Container, Row } from "react-bootstrap";
+import CardC from "../components/card/CardC";
+import clientAxios from "../helpers/axios.helpers";
 
 const ShopPage = () => {
+  const [productos, setProductos] = useState([]);
+
+  const obtenerProductos = async () => {
+    try {
+      const productos = await clientAxios.get("/productos");
+      setProductos(productos.data.productos);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    obtenerProductos();
+  }, []);
+
   return (
     <>
-      <div className="banner-tienda">
-        <h1> Imagen </h1>
-      </div>
-      <Container fluid>
-        <Row>
-          <Col sm="12" md="12" lg="3" className="col-filtros">
+      <div className="banner-tienda"></div>
+      <Container fluid className="container-shop">
+        <Row className="py-5">
+          <Col lg="3" className="col-filtros">
             Filtros
           </Col>
-          <Col sm="12" md="12" lg="9" className="col-productos">
-            asd
+
+          <Col lg="9" className="col-productos">
+            <Row>
+              {productos.map((producto) => (
+                <Col
+                  key={producto._id}
+                  sm="12"
+                  md="6"
+                  lg="4"
+                  className="d-flex justify-content-center py-3"
+                >
+                  <CardC
+                    idProd={producto._id}
+                    urlImagen={producto.imagen}
+                    titulo={producto.nombre}
+                    precio={producto.precio}
+                    descripcion={producto.descripcion}
+                  />
+                </Col>
+              ))}
+            </Row>
           </Col>
         </Row>
       </Container>
