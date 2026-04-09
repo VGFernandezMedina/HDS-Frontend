a0d3294b0d623c61eced086b9796c937bb7cd37d
Gabriel <gabrielfernandez090997@gmail.com>
2026-04-09 19:41:13 -0300

    Se agregó configHeaders y se vinculó el carrito del front con el back

--- src/helpers/axios.helpers.js
+++ src/helpers/axios.helpers.js
@@ -1,7 +1,18 @@
 import axios from "axios";
 
+const token = JSON.parse(localStorage.getItem("token")) || null;
+
 const clientAxios = axios.create({
   baseURL: `${import.meta.env.VITE_URL_BACK_LOCAL}/api`,
 });
+
+// Pasamos en la cabecera el auth que viene desde el backend para ingresar en las rutas protegidas
+
+export const configHeaders = {
+  headers: {
+    "content-type": "application/json",
+    auth: `${token}`,
+  },
+};
 
 export default clientAxios;
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,9 +1,130 @@
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./CartPage.css";
+import { useEffect, useState } from "react";
+import clientAxios, { configHeaders } from "../helpers/axios.helpers";
+import { FaRegTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const CartPage = () => {
+  const [productos, setProductos] = useState([]);
+
+  const obtenerProductoDelCarrito = async () => {
+    const res = await clientAxios.get(
+      "/carritos/obtenerProductos",
+      configHeaders,
+    );
+    console.log(res.data);
+    setProductos(res.data.productos);
+  };
+
+  const handleDelete = async (idProducto) => {
+    Swal.fire({
+      title: "Estas seguro de que quieres eliminar a este producto?",
+      text: "Si te arrepientes despues puedes cargarlo nuevamente!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "SI, estoy seguro!",
+    }).then(async (result) => {
+      try {
+        if (result.isConfirmed) {
+          const res = await clientAxios.put(
+            `/carritos/eliminarProducto/${idProducto}`,
+            {},
+            configHeaders,
+          );
+          if (res.status === 200) {
+            Swal.fire({
+              title: "Producto eliminado del carrito!",
+              text: "Tu producto fue eliminado con exito",
+              icon: "success",
+            });
+          }
+          obtenerProductoDelCarrito();
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  };
+
+  useEffect(() => {
+    obtenerProductoDelCarrito();
+  }, []);
+
   return (
     <>
-      <div>asd</div>
+      <Container fluid className="container-cart">
+        <Row>
+          <Col sm="12" md="6" lg="8" className="">
+            <div>
+              <div className="par-info">
+                <p>Producto</p>
+                <p>Precio</p>
+              </div>
+              <hr />
+            </div>
+            {productos &&
+              productos.map((producto) => (
+                <Row key={producto._id} className="w-100 pb-2">
+                  <Col sm="" md="" lg="3" className="col-cart border">
+                    <div className="div-img-cart">
+                      <img src="/camisetaHDS.jpg" alt="camisetaHDS" />
+                    </div>
+                  </Col>
+                  <Col sm="" md="" lg="4" className="col-cart border">
+                    <p className="m-0">{producto.nombre}</p>
+                  </Col>
+                  <Col sm="" md="" lg="2" className="col-cart border">
+                    <p className="m-0">cantidad</p>
+                  </Col>
+                  <Col sm="" md="" lg="3" className="col-cart border">
+                    <div className="cart-price-delete">
+                      <Button onClick={() => handleDelete(producto._id)}>
+                        <FaRegTrashAlt />
+                      </Button>
+                      <p className="m-0">${producto.precio}</p>
+                    </div>
+                  </Col>
+                  <hr className="mt-4" />
+                </Row>
+              ))}
+          </Col>
+          <Col sm="12" md="6" lg="4" className="border col-cart-info p-0">
+            <div className="div-cart-info border">
+              <h4>Resumen de compras</h4>
+              <hr />
+              <div className="d-flex flex-column ">
+                <div className="par-info">
+                  <p>Producto</p>
+                  <p>precio</p>
+                </div>
+                <hr />
+                <div className="par-info">
+                  <p>Envío</p>
+                  <p>precio</p>
+                </div>
+                <hr />
+                <div className="">
+                  <div className="par-info">
+                    <p>Agregar un cupón</p>
+                    <p>icono</p>
+                  </div>
+                </div>
+              </div>
+              <div className="total-info">
+                <hr />
+                <div className="par-info pb-3">
+                  <p>Total</p>
+                  <p>precio</p>
+                </div>
+                <Button>Continuar</Button>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
